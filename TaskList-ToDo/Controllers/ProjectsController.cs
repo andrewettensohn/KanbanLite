629dcaabdfca8f7fea313acf486a25320233b532
Andrew Ettensohn <andrewettensohn@gmail.com>
2019-12-15 19:29:00 -0500

    Locked board page behind auth

---
+++ TaskList-ToDo/Controllers/ProjectsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaskList_ToDo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TaskList_ToDo.Controllers
+{
+    [Authorize]
+    public class ProjectsController : Controller
+    {
+        private readonly ILogger<ProjectsController> _logger;
+
+        public IActionResult Board()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
