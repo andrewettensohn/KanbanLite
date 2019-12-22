4e731fc58caa672341140ffd0994f59636fa9e7e
Andrew Ettensohn <andrewettensohn@gmail.com>
2019-12-21 20:18:57 -0500

    Added identity files, style changes

---
+++ TaskList-ToDo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskList_ToDo.Data;
+
+[assembly: HostingStartup(typeof(TaskList_ToDo.Areas.Identity.IdentityHostingStartup))]
+namespace TaskList_ToDo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
